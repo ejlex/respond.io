7a80cc37c1404a65deb1e5bce2219e10a0be19ab
ejlex <jxlee0398@gmail.com>
2025-12-30 17:38:24 +0800

    install tailwind, query

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from 'vite-plugin-tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
 })
