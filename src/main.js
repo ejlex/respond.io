cdcdacb15e53f491c665fcb90fde8836032d0b1d
ejlex <jxlee0398@gmail.com>
2025-12-30 19:36:06 +0800

    queryClientConfig

--- src/main.js
+++ src/main.js
@@ -1,14 +1,27 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import { VueQueryPlugin } from '@tanstack/vue-query'
-import App from './App.vue'
-import router from './router'
-import './style.css'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { VueQueryPlugin } from "@tanstack/vue-query";
+import App from "./App.vue";
+import router from "./router";
+import "./style.css";
 
-const pinia = createPinia()
-const app = createApp(App)
+const pinia = createPinia();
+const app = createApp(App);
 
-app.use(VueQueryPlugin)
-app.use(pinia)
-app.use(router)
-app.mount('#app')
+const vueQueryPluginOptions = {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        networkMode: "always",
+        staleTime: Infinity,
+        gcTime: 60 * 60 * 1000,
+      },
+    },
+  },
+};
+
+app.use(VueQueryPlugin, vueQueryPluginOptions);
+app.use(pinia);
+app.use(router);
+app.mount("#app");
